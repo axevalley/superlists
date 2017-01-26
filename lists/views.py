0e6f301afed193399143230bab0080ae549dcbc1
Luke Shiner <luke@lukeshiner.com>
2017-01-26 20:54:12 +0000

    Rename all intem input ids and names. Still broken

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect, render
 from django.core.exceptions import ValidationError
+from lists.forms import ItemForm
 from lists.models import Item, List
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
 
 
 def view_list(request, list_id):
@@ -12,18 +13,19 @@ def view_list(request, list_id):
     error = None
     if request.method == 'POST':
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
+            return render(request, 'home.html', {"error": error})
     return render(request, 'list.html', {'list': list_, 'error': error})
 
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item.objects.create(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
